4bf7f172cd4eaddbcba8557c2fc1102b6548f182
JoaquinAglr <aguilar.jqn@gmail.com>
2025-10-01 08:59:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # URL permanente del CSV en GitHub (sin token)
 GITHUB_URL = "https://github.com/JoaquinAglr/buscador-relevamiento/raw/main/relevamiento.csv"
-df = pd.read_csv(url, delimiter=',')
+# df = pd.read_csv(url, delimiter=',')
 
 st.set_page_config(page_title="Buscador de Relevamiento", layout="wide")
 
@@ -38,6 +38,7 @@ if query:
     st.dataframe(resultados)
 else:
     st.info("Ingrese un término de búsqueda en la barra lateral.")
+
 
 
 
